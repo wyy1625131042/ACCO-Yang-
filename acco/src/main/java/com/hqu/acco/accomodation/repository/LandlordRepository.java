e2f3ffb812c9cdbcd8b3187bdad69c2983302abc
wyy1625131042 <38305962+wyy1625131042@users.noreply.github.com>
2018-07-07 00:43:08 +0800

    1

--- acco/src/main/java/com/hqu/acco/accomodation/repository/LandlordRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hqu.acco.accomodation.repository;
-
-import com.hqu.acco.accomodation.entity.Landlord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LandlordRepository extends JpaRepository<Landlord,Integer> {
-    Landlord findByLoginName(String s);
-}
